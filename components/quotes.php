4697919617512a4a1f615033f08cd51046d8734a
xenia <bogd.kseniya050199@mail.ru>
2022-06-23 20:53:56 +0300

    первый сайт на пхп

---
+++ components/quotes.php
@@ -0,0 +1,15 @@
+<section class="quotes container">
+  <h2 class="section-title">Любимые цитаты</h2>
+  <blockquote>
+    <p>Плохие программисты думают о коде. Хорошие программисты думают о структурах данных и их взаимосвязях.</p>
+    <cite>Линус Торвальдс</cite>
+  </blockquote>
+  <blockquote>
+    <p>Отпутите меня на поспать.</p>
+    <cite>Альпака</cite>
+  </blockquote>
+  <blockquote>
+    <p>схожу к Цою на концерт.</p>
+    <cite>Иван Ерофеев</cite>
+  </blockquote>
+</section>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$title = 'Приключения Дурного Оленя' ;
+$name = 'Дурной Олень';
+$image = 'img/crazy-deer.svg';
+$email = 'ksenbogdanova05@gmail.com';
+$phone = '+79771233344';
+
+require('components/header.php');
+
+require('components/about.php');
+require('components/photos.php');
+require('components/quotes.php');
+require('components/status.php');
+require('components/news.php');
+
+
+
+require('components/contacts.php');
+require('components/footer.php');
